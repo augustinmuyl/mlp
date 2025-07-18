eb77a3a55a221d2b6de7eb74fec79801f2f54e35
Augustin <augustinusmp@gmail.com>
2025-07-18 21:04:36 +0200

    added training bar

--- trainer.py
+++ trainer.py
@@ -3,6 +3,7 @@ from model import MLP
 from sklearn.datasets import make_moons
 from sklearn.model_selection import train_test_split
 from visualization import plot_loss_terminal, plot_predictions, plot_predictions_terminal
+from rich.progress import Progress, BarColumn, TextColumn, TimeRemainingColumn
 
 
 def train_model(hidden_layers, lr, epochs, use_terminal_plot, patience=100):
@@ -23,40 +24,56 @@ def train_model(hidden_layers, lr, epochs, use_terminal_plot, patience=100):
     training_losses = []
     test_losses = []
 
-    for i in range(epochs):
-        # Training
-        y_pred_train = model.forward(X_train)
-        y_pred_train = np.clip(y_pred_train, 1e-9, 1 - 1e-9)
-        loss_train = np.mean(
-            -(y_train * np.log(y_pred_train) + (1 - y_train) * np.log(1 - y_pred_train))
+    with Progress(
+        TextColumn("[bold green]{task.description}"),
+        BarColumn(),
+        TextColumn("[progress.percentage]{task.percentage:>3.0f}%"),
+        TextColumn(" • loss={task.fields[loss]:.4f}"),
+        TextColumn(" • acc={task.fields[acc]:.2%}"),
+        TimeRemainingColumn(),
+        transient=True,
+    ) as progress:
+        task = progress.add_task(
+            "[green]Training[/green]", total=epochs, loss=float("nan"), acc=float("nan")
         )
-        training_losses.append(loss_train)
 
-        model.backward(y_train, y_pred_train, lr)
+        for i in range(epochs):
+            # Training
+            y_pred_train = model.forward(X_train)
+            y_pred_train = np.clip(y_pred_train, 1e-9, 1 - 1e-9)
+            loss_train = np.mean(
+                -(y_train * np.log(y_pred_train) + (1 - y_train) * np.log(1 - y_pred_train))
+            )
+            training_losses.append(loss_train)
 
-        # Testing
-        y_pred_test = model.forward(X_test)
-        y_pred_test = np.clip(y_pred_test, 1e-9, 1 - 1e-9)
-        loss_test = np.mean(
-            -(y_test * np.log(y_pred_test) + (1 - y_test) * np.log(1 - y_pred_test))
-        )
-        test_losses.append(loss_test)
-        acc = np.mean(y_test == np.rint(y_pred_test))
+            model.backward(y_train, y_pred_train, lr)
 
-        if loss_test <= best_test_loss:
-            best_test_loss = loss_test
-            best_loss_acc = acc
-            count = 0
-        else:
-            count += 1
-            if count == patience:
-                break
+            # Testing
+            y_pred_test = model.forward(X_test)
+            y_pred_test = np.clip(y_pred_test, 1e-9, 1 - 1e-9)
+            loss_test = np.mean(
+                -(y_test * np.log(y_pred_test) + (1 - y_test) * np.log(1 - y_pred_test))
+            )
+            test_losses.append(loss_test)
+            acc = np.mean(y_test == np.rint(y_pred_test))
+
+            if loss_test <= best_test_loss:
+                best_test_loss = loss_test
+                best_loss_acc = acc
+                count = 0
+            else:
+                count += 1
+                if count == patience:
+                    break
+
+            progress.update(task, advance=1, loss=loss_test, acc=acc)
 
     y_pred_test = model.forward(X_test)
 
     if use_terminal_plot:
         plot_predictions_terminal(X_test, y_pred_test)
     else:
-        plot_predictions(X_test, y_pred_test)
+        # plot_predictions(X_test, y_pred_test)
+        pass
 
     return best_test_loss, best_loss_acc
